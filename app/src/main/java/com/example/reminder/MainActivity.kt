dda0d97f4944c78e984a15c7640c7ebedf9ad9bb
Edyard Karastoyka <88448179+MisterEdinson@users.noreply.github.com>
2023-03-16 00:18:42 +0500

    add fragment
    list
    add
    update
    and navigation

--- app/src/main/java/com/example/reminder/MainActivity.kt
+++ app/src/main/java/com/example/reminder/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.reminder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupActionBarWithNavController(findNavController(R.id.navHostFragment))
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navHostFragment = findNavController(R.id.navHostFragment)
+        return navHostFragment.navigateUp() || super.onSupportNavigateUp()
     }
 }
---
+++ app/src/main/java/com/example/reminder/screens/list/ListFragment.kt
@@ -0,0 +1,28 @@
+package com.example.reminder.screens.list
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.reminder.R
+import kotlinx.android.synthetic.main.fragment_list.view.*
+
+class ListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
+        view.floatingActionButton.setOnClickListener{
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+        view.listLayout.setOnClickListener{
+            findNavController().navigate(R.id.action_listFragment_to_updateFragment)
+        }
+        return view
+    }
+}
