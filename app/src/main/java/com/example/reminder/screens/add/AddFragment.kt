a69349f605db21ae388c0f71f84cf38fd50ba8cc
Edyard Karastoyka <88448179+MisterEdinson@users.noreply.github.com>
2023-03-16 00:59:57 +0500

    add 2 menu fragment
    update
    add

--- app/src/main/java/com/example/reminder/screens/add/AddFragment.kt
+++ app/src/main/java/com/example/reminder/screens/add/AddFragment.kt
@@ -1,10 +1,9 @@
 package com.example.reminder.screens.add
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.setFragmentResult
 import com.example.reminder.R
 
 class AddFragment : Fragment() {
@@ -13,8 +12,13 @@ class AddFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        setHasOptionsMenu(true)
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_menu,menu)
     }
 
 }
